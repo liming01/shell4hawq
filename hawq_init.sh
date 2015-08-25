14e404a513abcb1b2a9cc0f3785121aacb5030ae
Ming LI <mli@pivotal.io>
2015-08-25 19:12:16 +0800

    rm temp directories for hawq_init

--- hawq_init.sh
+++ hawq_init.sh
@@ -41,6 +41,7 @@ source $GPHOME/greenplum_path.sh
 
 rm -rf $HAWQ_DATA_DIR/data/master $HAWQ_DATA_DIR/data/primary $HAWQ_DATA_DIR/data1/primary $HAWQ_DATA_DIR/data2/primary
 mkdir -p $HAWQ_DATA_DIR/data/master $HAWQ_DATA_DIR/data/primary $HAWQ_DATA_DIR/data1/primary $HAWQ_DATA_DIR/data2/primary
+rm -rf $HOME/temp/hawq1/data1/tmp1 $HOME/temp/hawq1/data1/tmp2 $HOME/temp/hawq1/data2/tmp1 $HOME/temp/hawq1/data2/tmp2
 mkdir -p $HOME/temp/hawq1/data1/tmp1 $HOME/temp/hawq1/data1/tmp2 $HOME/temp/hawq1/data2/tmp1 $HOME/temp/hawq1/data2/tmp2
 
 rm -rf /tmp/.s.PGSQL.*.lock
