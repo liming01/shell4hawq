e1e915e7c6c6beec243131a497b518bb4772e673
Ming LI <mli@apache.org>
2017-06-20 22:46:45 -0700

    different sed param for different os

--- gpdb/pre.sh
+++ gpdb/pre.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sed -i ''  '15,23 s/^/#/' ~/workspace/install/gpdb/bin/lib/gp_bash_functions.sh
-sed -i ''  '28,36 s/^/#/' ~/workspace/install/gpdb4/bin/lib/gp_bash_functions.sh
+if [ "Darwin" == $(uname -s) ]; then
+	echo 'on mac'
+	sed -i ''  '15,23 s/^/#/' ~/workspace/install/gpdb/bin/lib/gp_bash_functions.sh
+	sed -i ''  '28,36 s/^/#/' ~/workspace/install/gpdb4/bin/lib/gp_bash_functions.sh
+else
+	echo 'on Linux'
+	sed -i '15,23 s/^/#/' ~/workspace/install/gpdb/bin/lib/gp_bash_functions.sh
+	sed -i '28,36 s/^/#/' ~/workspace/install/gpdb4/bin/lib/gp_bash_functions.sh
+fi
